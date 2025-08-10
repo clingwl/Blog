4c1f7cc71f6b2ee44a1ec6c0a4bd6d59cc890bbe
clingwl <admin@jieyou.ltd>
2025-08-10 22:32:25 +0800

    提交说明

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,5 @@
+
+export default new Map([
+["src/content/blog/frosti-mdx.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Ffrosti-mdx.mdx&astroContentModuleFlag=true")],
+["src/content/blog/using-mdx.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fblog%2Fusing-mdx.mdx&astroContentModuleFlag=true")]]);
+		
